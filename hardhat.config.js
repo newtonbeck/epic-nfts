7a2fdb4b47e6140aff707439912eba7af0c94e65
Newton Rhomel Beck Angelini <newton.beck@personio.de>
2022-10-22 14:47:36 -0300

    Adds hardhat verification on etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,5 +9,8 @@ module.exports = {
       url: process.env.QUICKNODE_API_KEY_URL,
       accounts: [process.env.GOERLI_PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
